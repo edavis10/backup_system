e48b19aeb94105bb6dfcb507e2ec5277b4749bbd
Eric Davis <edavis@littlestreamsoftware.com>
2012-02-20 11:18:57 -0800

    Generated Rails 3.2.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackupSystem::Application.config.secret_token = '45abb9fde5b65ac4db661879c794807493e9ec5ecbe062a87a933535719c1b7d44362e158208189d88d2ac7929ad82af1dae168adaf57345a2685c1e8aff97ae'
