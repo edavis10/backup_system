5dbe99b5e8711d4a335e7001d9891f70a3754edb
Eric Davis <edavis@littlestreamsoftware.com>
2012-02-20 17:44:30 -0800

    Make sure backup logs are getting, well, logged

--- test/integration/creating_backup_test.rb
+++ test/integration/creating_backup_test.rb
@@ -4,10 +4,10 @@ class CreatingBackupTest < ActionDispatch::IntegrationTest
   test "should allow creating a new backup on a host using the API" do
     @host = Host.create!(:name => 'test.example.com')
 
-    backup = {"log" => "The backup was successful"}
-    page.driver.post host_backups_path(@host, :format => "json"), backup.to_json
+    page.driver.post host_backups_path(@host, :format => "json"), "backup" => {"log" => "The backup was successful"}
     
     assert_equal 1, @host.backups.count
+    assert_equal "The backup was successful", @host.backups.first.log
   end
   
 end
