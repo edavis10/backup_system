a573f3437a479456f86e75bb3cd7a1af721df7f3
Eric Davis <edavis@littlestreamsoftware.com>
2012-02-22 12:10:05 -0800

    Create sorcery scaffolding for users and login/logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120221000159) do
+ActiveRecord::Schema.define(:version => 20120222194055) do
 
   create_table "backups", :force => true do |t|
     t.text     "log"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(:version => 20120221000159) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",            :null => false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
